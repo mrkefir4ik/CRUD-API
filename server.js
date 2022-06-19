536b532ed275d0885ed6c5e0761e0cd8914fed2e
Daniil-Kandasoft <daniil.ahorodnikou@kandasoft.com>
2022-06-19 16:32:53 +0300

    fix: fixint git

---
+++ server.js
@@ -0,0 +1,67 @@
+import http from "http";
+import { getUsers, getUserByID, createUser, updateUser, removeUser } from './controllers/userController.js'
+import { validate } from "uuid";
+
+const server = http.createServer((req, res) => {
+
+    process.on("uncaughtException", () => {
+        res.writeHead(500, { 'Context-type': 'application/json' })
+        res.end(JSON.stringify({ message: 'Oops, something went wrong' }))
+    });
+
+    if (req.url === '/api/users' && req.method === 'GET') {
+        getUsers(req, res);
+    }
+
+    else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'GET') {
+
+        const id = req.url.split('/')[3]
+
+        if (!validate(id)) {
+            res.writeHead(400, { 'Context-type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Invalid ID Format' }))
+        }
+
+        getUserByID(req, res, id);
+
+        // res.end(JSON.stringify())
+    }
+
+    else if (req.url === '/api/users' && req.method === 'POST') {
+        createUser(req, res);
+    }
+
+    else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'PUT') {
+        const id = req.url.split('/')[3];
+
+        if (!validate(id)) {
+            res.writeHead(400, { 'Context-type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Invalid ID Format' }))
+        }
+
+        updateUser(req, res, id);
+    }
+
+    else if (req.url.match(/\/api\/users\/\w+/) && req.method === 'DELETE') {
+        const id = req.url.split('/')[3];
+
+        if (!validate(id)) {
+            res.writeHead(400, { 'Context-type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Invalid ID Format' }))
+        }
+
+        removeUser(req, res, id);
+    }
+
+    else {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: 'Route Not Found' }))
+    }
+
+});
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
